e7c17966cdafd101f603402f2dd856985f109077
IshaniAnurag <67219467+IshaniAnurag@users.noreply.github.com>
2020-08-01 13:39:53 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,23 @@
+class box{
+    constructor(x,y) {
+        var options = {
+            'restitution':0.2,
+            'friction':0.6,
+            'density':1.5
+        }
+        this.body = Bodies.rectangle(x, y, 30, 40, options);
+        this.width = 30;
+        this.height = 40;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("pink");
+        rect(0,0, this.width, this.height);
+        pop();
+      }
+}
